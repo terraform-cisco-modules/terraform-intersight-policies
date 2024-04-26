6fd2de5898ef4c73ef71abe4d014837eae51fe0f
scotttyso <tyscott@cisco.com>
2024-04-25 20:24:42 -0400

    updating provider to v1.0.47.

--- network_connectivity.tf
+++ network_connectivity.tf
@@ -12,7 +12,7 @@ resource "intersight_networkconfig_policy" "map" {
   dynamic_dns_domain       = each.value.update_domain
   enable_dynamic_dns       = each.value.enable_dynamic_dns
   enable_ipv4dns_from_dhcp = each.value.obtain_ipv4_dns_from_dhcp
-  enable_ipv6              = each.value.enable_ipv6
+  enable_ipv6              = each.value.obtain_ipv6_dns_from_dhcp == true || length(each.value.dns_servers_v6) > 0 ? true : false
   enable_ipv6dns_from_dhcp = each.value.obtain_ipv6_dns_from_dhcp
   preferred_ipv4dns_server = length(each.value.dns_servers_v4) > 0 ? each.value.dns_servers_v4[0] : "0.0.0.0"
   preferred_ipv6dns_server = length(each.value.dns_servers_v6) > 0 ? each.value.dns_servers_v6[0] : "::"
